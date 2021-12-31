743702080c1b154a2b52389d8a1419c3b99a2847
Perry Xie <perry.xie1@gmail.com>
2021-12-31 15:02:34 -0800

    added input to sidebar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {config} from '../config'
 import {loadMap} from './scripts/loadmap'
 
+// console.log('sanity check, index.js')
+
 // Create the script tag, set the appropriate attributes
 const script = document.createElement('script');
 script.src = `https://maps.googleapis.com/maps/api/js?key=${config.api}&callback=initMap`;
@@ -11,5 +13,5 @@ document.head.appendChild(script);
 
 // Attach your callback function to the `window` object
 window.initMap = function() {
-  loadMap()
+  loadMap();
 };
--- src/scripts/loadmap.js
+++ src/scripts/loadmap.js
@@ -1,5 +1,5 @@
 function loadMap() {
-  console.log('sane')
+  console.log('sanity check, loadmap')
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
