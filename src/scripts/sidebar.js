3b4b5d5a9926646fcd99ea6539b23957456867fb
Perry Xie <perry.xie1@gmail.com>
2021-12-31 19:13:51 -0800

    bug fix, limit user to 10 inputs

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -3,23 +3,23 @@ console.log('sanity check, sidebar')
 const addressUl = document.querySelector(".address-list")
 const searchDiv = document.querySelector('.search-div');
 
-// Enables next input field and dynamically appends a clone underneath.
+// Enables next input field and dynamically appends a input bar underneath.
 function handleNewInput(){
   
-  
-  // adds eventlistener to parent and checks if target's parent is li.li.input-additional
+  // creates a new input bar
   addressUl.addEventListener('click', e => {
 
     if (e.target && e.target.parentElement.matches('li.input-additional') ) {
       const inputLi = document.querySelector(".input-additional"); 
       const newDisabledLi = inputLi.cloneNode(true);
-
-      inputLi.classList.remove('input-additional');
-
-      // limit user to only 10 entries 
-      if (document.querySelectorAll('.address-list li').length !== 10) {
-        addressUl.appendChild(newDisabledLi);
+      inputLi.classList.remove('input-additional');  // undisables current li
+      
+      // limit user to only 10 entries
+      // it hits 10, adds a hidden class to last element (used in handleDelete Input)
+      if (document.querySelectorAll('.address-list li').length === 10) {
+        newDisabledLi.classList.add('hidden')
       }
+      addressUl.appendChild(newDisabledLi);
     }
   });
 }
@@ -27,14 +27,18 @@ function handleNewInput(){
 // deletes li that hosts the input
 function handleDeleteInput() {
 
+  // removes parent li 
   addressUl.addEventListener('click', e=>{
-    console.log(e.target);
     if (e.target && e.target.matches('button.remove-address')) {
       e.target.parentNode.parentNode.remove();
+
+      // If there is 9 showing elements, unhides hidden element
+      if (document.querySelectorAll('.address-list li').length === 10) {
+        const hiddenLi = document.querySelector(".hidden"); 
+        hiddenLi.classList.remove('hidden');
+      }
     }
   })
-
-
 }
 
 export {handleNewInput, handleDeleteInput}
